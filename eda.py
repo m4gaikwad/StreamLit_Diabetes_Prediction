5ff7f837843a04690791ddf7cc98496091e17ea4
Mayur <m4gaikwad@gmail.com>
2022-05-20 11:47:57 +0530

    Added functions for obtaining plots and dataframes

--- eda.py
+++ eda.py
@@ -13,6 +13,61 @@ def load_data(dataframe):
         return df
 
 
+def count_dataframe(dataframe, column):
+    new_df = dataframe[column].value_counts().to_frame()
+    new_df = new_df.reset_index()
+    new_df.columns = [column, 'Count']
+    return new_df
+
+
+def show_summary(df, data_menu):
+    for i in df.columns:
+        if data_menu == i:
+            try:
+                st.dataframe(df[i].describe())
+            except StreamlitAPIException:
+                st.error('Selected Column is Categorical.'
+                         'Please Select Column with '
+                         'continuous Numerical Data')
+
+
+def plot_distribution_bar(df, data_menu):
+    for i in df.columns:
+        try:
+            if data_menu == i:
+                try:
+                    fig = plt.figure()
+                    sns.countplot(df[i])
+                    st.pyplot(fig)
+
+                    new_df = count_dataframe(df, i)
+                    st.dataframe(new_df)
+
+                except StreamlitAPIException:
+                    st.error('Select Another Column')
+            else:
+                continue
+        except AttributeError:
+            st.write('Upload Correct File')
+
+
+def plot_count_pie(df, data_menu):
+    for i in df.columns:
+        try:
+            if data_menu == i:
+                try:
+                    new_df = count_dataframe(df, i)
+                    pi = px.pie(new_df, names=i, values='Count')
+                    st.plotly_chart(pi, use_container_width=True)
+
+                except StreamlitAPIException:
+                    st.error('Select Another Column')
+            else:
+                continue
+        except AttributeError:
+            st.write('Upload Correct File')
+
+
 def show_data():
     data = st.file_uploader('Upload CSV Data File', type='csv',
                             accept_multiple_files=True)
@@ -23,7 +78,7 @@ def show_data():
 
     if df is not None:
         data_menu = st.sidebar.selectbox('Data Columns',
-                                        df.columns)
+                                         df.columns)
 
     if submenu == 'Descriptive':
         st.subheader('Descriptive Summary')
@@ -34,19 +89,12 @@ def show_data():
                 st.table(dt)
 
             with st.expander("Descriptive Summary"):
-                for i in df.columns:
-                    if data_menu == i:
-                        try:
-                            st.dataframe(df[i].describe())
-                        except StreamlitAPIException:
-                            st.error('Selected Column is Categorical.'
-                                     'Please Select Column with '
-                                     'continuous Numerical Data')
+                show_summary(df, data_menu)
 
             with st.expander("Class Distribution"):
                 for i in df.columns:
                     if data_menu == i:
-                        st.dataframe(df[i].value_counts())
+                        st.dataframe(count_dataframe(df, i))
 
     elif submenu == 'Plot':
         st.subheader('Plot')
@@ -55,44 +103,8 @@ def show_data():
             col1, col2 = st.columns([1, 1])
             with col1:
                 with st.expander('Distribution'):
-                    for i in df.columns:
-                        try:
-
-                            if data_menu == i:
-                                try:
-                                    fig = plt.figure()
-                                    sns.countplot(df[i])
-                                    st.pyplot(fig)
-
-                                    new_df = df[i].value_counts().to_frame()
-                                    new_df = new_df.reset_index()
-                                    new_df.columns = [i, 'Count']
-                                    st.dataframe(new_df)
-
-                                except StreamlitAPIException:
-                                    st.error('Select Another Column')
-                            else:
-                                continue
-                        except AttributeError:
-                            st.write('Upload Correct File')
+                    plot_distribution_bar(df, data_menu)
 
             with col2:
                 with st.expander('Distribution Pie Chart'):
-                    for i in df.columns:
-                        try:
-                            if data_menu == i:
-                                try:
-                                    new_df = df[i].value_counts().to_frame()
-                                    new_df = new_df.reset_index()
-                                    new_df.columns = [i, 'Count']
-                                    #st.dataframe(new_df)
-
-                                    pi = px.pie(new_df, names=i, values='Count')
-                                    st.plotly_chart(pi, use_container_width=True)
-
-                                except StreamlitAPIException:
-                                    st.error('Select Another Column')
-                            else:
-                                continue
-                        except AttributeError:
-                            st.write('Upload Correct File')
+                    plot_count_pie(df, data_menu)
