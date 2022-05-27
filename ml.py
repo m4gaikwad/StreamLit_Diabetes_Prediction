c2df917f0e6cda393c0e2a54c0416d9f0fb845c9
Mayur <m4gaikwad@gmail.com>
2022-05-27 14:45:56 +0530

    Added Model Selection in Sidebar

--- ml.py
+++ ml.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 import joblib
 import os
+
+import numpy as np
 attrib_info = """
 #### Attribute Information:
     - Age 1.20-65
@@ -50,9 +52,19 @@ def load_model(model):
     loaded_model = joblib.load(open(os.path.join(model),'rb'))
     return loaded_model
 
+def diagnosis(predict,prediction):
+    if predict == 1:
+        st.warning('Positive Risk: {} %'.format(round(prediction[0][1] * 100),2))
+        st.warning('You are at risk of getting Diabetes.')
+    else:
+        st.info('Negative: {} %'.format(round(prediction[0][0] * 100),2))
+        st.info('Congratulations. You are healthy.')
+
+
+
 def calculate():
     submenu = ['Logistic Regression', 'Decision Tree']
-    model = st.sidebar.selectbox('Model',submenu)
+    models = st.sidebar.selectbox('Model',submenu)
 
     with st.expander('Attribute Info'):
         st.markdown(attrib_info)
@@ -61,45 +73,45 @@ def calculate():
     col1,col2 = st.columns(2)
 
     with col1:
-        age = st.number_input('Age',min_value=10,max_value=100)
-        gender = st.radio('Gender',['Female','Male'])
-        polyuria = st.radio('Polyuria', ['Yes', 'No'])
-        polydipsia = st.radio('Polydipsia', ['Yes', 'No'])
-        sudden_weight_loss= st.radio('Sudden Weight Loss', ['Yes', 'No'])
-        weakness = st.selectbox('Weakness', ['Yes', 'No'])
-        polyphagia = st.selectbox('Polyphagia', ['Yes', 'No'])
-        genital_thrush = st.select_slider('Genital Thrush', ['Yes', 'No'])
+        age = st.number_input("Age", 10, 100)
+        gender = st.radio("Gender", ("Female", "Male"))
+        polyuria = st.radio("Polyuria", ["No", "Yes"])
+        polydipsia = st.radio("Polydipsia", ["No", "Yes"])
+        sudden_weight_loss = st.selectbox("Sudden_weight_loss", ["No", "Yes"])
+        weakness = st.radio("weakness", ["No", "Yes"])
+        polyphagia = st.radio("polyphagia", ["No", "Yes"])
+        genital_thrush = st.selectbox("Genital_thrush", ["No", "Yes"])
 
 
     with col2:
-        visual_blurring = st.selectbox('Visual Blurring', ['Yes', 'No'])
-        itching = st.radio('Itching', ['Yes', 'No'])
-        irritability = st.radio('Irritability', ['Yes', 'No'])
-        delayed_healing = st.radio('Delayed Healing', ['Yes', 'No'])
-        partial_paresis = st.radio('Partial Paresis', ['Yes', 'No'])
-        muscle_stiffness = st.radio('Muscle Stiffness', ['Yes', 'No'])
-        alopecia = st.select_slider('Alopecia', ['Yes', 'No'])
-        obesity = st.selectbox('Obesity', ['Yes', 'No'])
+        visual_blurring = st.selectbox("Visual_blurring", ["No", "Yes"])
+        itching = st.radio("itching", ["No", "Yes"])
+        irritability = st.radio("irritability", ["No", "Yes"])
+        delayed_healing = st.radio("delayed_healing", ["No", "Yes"])
+        partial_paresis = st.selectbox("Partial_paresis", ["No", "Yes"])
+        muscle_stiffness = st.radio("muscle_stiffness", ["No", "Yes"])
+        alopecia = st.radio("alopecia", ["No", "Yes"])
+        obesity = st.select_slider("obesity", ["No", "Yes"])
 
     submit =st.button('Submit')
     if submit:
         with st.expander('Selected Options Are:'):
-            result = {'age': age,
-                      'gender': gender,
-                      'polyuria': polyuria,
-                      'polydipsia': polydipsia,
-                      'sudden_weight_loss':sudden_weight_loss,
-                      'weakness':weakness,
-                      'polyphagia':polyphagia,
-                      'genital_thrush':genital_thrush,
-                      'visual_blurring':visual_blurring,
-                      'itching':itching,
-                      'irritability':irritability,
-                      'delayed_healing':delayed_healing,
-                      'partial_paresis':partial_paresis,
-                      'muscle_stiffness':muscle_stiffness,
-                      'alopecia':alopecia,
-                      'obesity':obesity
+            result = {'age':age,
+                    'gender':gender,
+                    'polyuria':polyuria,
+                    'polydipsia':polydipsia,
+                    'sudden_weight_loss':sudden_weight_loss,
+                    'weakness':weakness,
+                    'polyphagia':polyphagia,
+                    'genital_thrush':genital_thrush,
+                    'visual_blurring':visual_blurring,
+                    'itching':itching,
+                    'irritability':irritability,
+                    'delayed_healing':delayed_healing,
+                    'partial_paresis':partial_paresis,
+                    'muscle_stiffness':muscle_stiffness,
+                    'alopecia':alopecia,
+                    'obesity':obesity
                       }
             st.write(result)
 
@@ -109,7 +121,26 @@ def calculate():
                     encoded_result.append(i)
                 elif i in ['Female','Male']:
                     res = get_value(i,gender_map)
+                    encoded_result.append(res)
                 else:
                     encoded_result.append(get_fvalue(i))
 
-            st.write(encoded_result)
+            #st.write(encoded_result)
+
+        with st.expander('Prediction Results'):
+            single_sample = np.array(encoded_result).reshape(1,-1)
+
+            if models == 'Logistic Regression':
+                model = load_model('model/logistic_regression_model_diabetes_21_oct_2020.pkl')
+                predict = model.predict(single_sample)
+                prediction = model.predict_proba(single_sample)
+                #st.write(prediction)
+                diagnosis(predict,prediction)
+            elif models == 'Decision Tree':
+                model = load_model('model/decision_tree_model_diabetes_21_oct_2020.pkl')
+                predict = model.predict(single_sample)
+                prediction = model.predict_proba(single_sample)
+                #st.write(prediction)
+                diagnosis(predict,prediction)
+            else:
+                st.error('Error')
