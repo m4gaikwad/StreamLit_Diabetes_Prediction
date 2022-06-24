031ccc169f39df2530de424955724928a82645ed
Mayur <m4gaikwad@gmail.com>
2022-06-24 12:20:16 +0530

    Added Requirements

--- ml.py
+++ ml.py
@@ -4,6 +4,7 @@ import joblib
 import os
 
 import numpy as np
+
 attrib_info = """
 #### Attribute Information:
     - Age 1.20-65
@@ -25,9 +26,9 @@ attrib_info = """
     - Class 1.Positive, 2.Negative.
 
 """
-label_dict = {'No':0,'Yes':1}
-gender_map = {'Female':0,'Male':1}
-target_label_map = {'Negative':0,'Positive':1}
+label_dict = {'No': 0, 'Yes': 1}
+gender_map = {'Female': 0, 'Male': 1}
+target_label_map = {'Negative': 0, 'Positive': 1}
 
 """
 'age','gender','polyuria','polydipsia','sudden_weight_loss',
@@ -36,41 +37,44 @@ target_label_map = {'Negative':0,'Positive':1}
  'muscle_stiffness','alopecia','obesity','class']
 """
 
+
 def get_fvalue(val):
     feature_dict = label_dict
-    for key,value in feature_dict.items():
+    for key, value in feature_dict.items():
         if val == key:
             return value
 
-def get_value(val,my_dict):
-    for key,value in my_dict.items():
+
+def get_value(val, my_dict):
+    for key, value in my_dict.items():
         if val == key:
             return value
+
 
 @st.cache
 def load_model(model):
-    loaded_model = joblib.load(open(os.path.join(model),'rb'))
+    loaded_model = joblib.load(open(os.path.join(model), 'rb'))
     return loaded_model
 
-def diagnosis(predict,prediction):
+
+def diagnosis(predict, prediction):
     if predict == 1:
-        st.warning('Positive Risk: {} %'.format(round(prediction[0][1] * 100),2))
+        st.warning('Positive Risk: {} %'.format(round(prediction[0][1] * 100), 2))
         st.warning('You are at risk of getting Diabetes.')
     else:
-        st.info('Negative: {} %'.format(round(prediction[0][0] * 100),2))
+        st.info('Negative: {} %'.format(round(prediction[0][0] * 100), 2))
         st.info('Congratulations. You are healthy.')
-
 
 
 def calculate():
     submenu = ['Logistic Regression', 'Decision Tree']
-    models = st.sidebar.selectbox('Model',submenu)
+    models = st.sidebar.selectbox('Model', submenu)
 
     with st.expander('Attribute Info'):
         st.markdown(attrib_info)
 
-    #Layout
-    col1,col2 = st.columns(2)
+    # Layout
+    col1, col2 = st.columns(2)
 
     with col1:
         age = st.number_input("Age", 10, 100)
@@ -82,7 +86,6 @@ def calculate():
         polyphagia = st.radio("polyphagia", ["No", "Yes"])
         genital_thrush = st.selectbox("Genital_thrush", ["No", "Yes"])
 
-
     with col2:
         visual_blurring = st.selectbox("Visual_blurring", ["No", "Yes"])
         itching = st.radio("itching", ["No", "Yes"])
@@ -93,25 +96,25 @@ def calculate():
         alopecia = st.radio("alopecia", ["No", "Yes"])
         obesity = st.select_slider("obesity", ["No", "Yes"])
 
-    submit =st.button('Submit')
+    submit = st.button('Submit')
     if submit:
         with st.expander('Selected Options Are:'):
-            result = {'age':age,
-                    'gender':gender,
-                    'polyuria':polyuria,
-                    'polydipsia':polydipsia,
-                    'sudden_weight_loss':sudden_weight_loss,
-                    'weakness':weakness,
-                    'polyphagia':polyphagia,
-                    'genital_thrush':genital_thrush,
-                    'visual_blurring':visual_blurring,
-                    'itching':itching,
-                    'irritability':irritability,
-                    'delayed_healing':delayed_healing,
-                    'partial_paresis':partial_paresis,
-                    'muscle_stiffness':muscle_stiffness,
-                    'alopecia':alopecia,
-                    'obesity':obesity
+            result = {'age': age,
+                      'gender': gender,
+                      'polyuria': polyuria,
+                      'polydipsia': polydipsia,
+                      'sudden_weight_loss': sudden_weight_loss,
+                      'weakness': weakness,
+                      'polyphagia': polyphagia,
+                      'genital_thrush': genital_thrush,
+                      'visual_blurring': visual_blurring,
+                      'itching': itching,
+                      'irritability': irritability,
+                      'delayed_healing': delayed_healing,
+                      'partial_paresis': partial_paresis,
+                      'muscle_stiffness': muscle_stiffness,
+                      'alopecia': alopecia,
+                      'obesity': obesity
                       }
             st.write(result)
 
@@ -119,28 +122,28 @@ def calculate():
             for i in result.values():
                 if type(i) == int:
                     encoded_result.append(i)
-                elif i in ['Female','Male']:
-                    res = get_value(i,gender_map)
+                elif i in ['Female', 'Male']:
+                    res = get_value(i, gender_map)
                     encoded_result.append(res)
                 else:
                     encoded_result.append(get_fvalue(i))
 
-            #st.write(encoded_result)
+            # st.write(encoded_result)
 
         with st.expander('Prediction Results'):
-            single_sample = np.array(encoded_result).reshape(1,-1)
+            single_sample = np.array(encoded_result).reshape(1, -1)
 
             if models == 'Logistic Regression':
                 model = load_model('model/logistic_regression_model_diabetes_21_oct_2020.pkl')
                 predict = model.predict(single_sample)
                 prediction = model.predict_proba(single_sample)
-                #st.write(prediction)
-                diagnosis(predict,prediction)
+                # st.write(prediction)
+                diagnosis(predict, prediction)
             elif models == 'Decision Tree':
                 model = load_model('model/decision_tree_model_diabetes_21_oct_2020.pkl')
                 predict = model.predict(single_sample)
                 prediction = model.predict_proba(single_sample)
-                #st.write(prediction)
-                diagnosis(predict,prediction)
+                # st.write(prediction)
+                diagnosis(predict, prediction)
             else:
                 st.error('Error')
